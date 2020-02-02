f52248ae052179733e793487ab48b5a4bcb70e65
Delai21 <k.gysin.kg@gmail.com>
2020-02-02 20:44:53 +0100

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Delai21/go-samples
+
+go 1.13
---
+++ pkg/command-line/cmd/cli/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	poker "github.com/Delai21/go-samples/pkg/command-line"
+)
+
+const dbFileName = "game.db.json"
+
+func main() {
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer close()
+
+	fmt.Println("Let's play poker")
+	fmt.Println("Type {Name} wins to record a win")
+	poker.NewCLI(store, os.Stdin).PlayPoker()
+}
